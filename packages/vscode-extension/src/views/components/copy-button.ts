d2ae4064f721c47ad15b2273401f9b4c7934ee08
Marvin <marvin-brouwer@live.nl>
2026-02-16 16:58:38 +0100

    Split button out

---
+++ packages/vscode-extension/src/views/components/copy-button.ts
@@ -0,0 +1,192 @@
+import chevronDownSvg from '@vscode/codicons/src/icons/chevron-down.svg'
+import copyIconSvg from '@vscode/codicons/src/icons/copy.svg'
+import { Button } from '@vscode/webview-ui-toolkit'
+
+// TODO client scripts will go here
+/*
+
+//https://github.com/microsoft/vscode-webview-ui-toolkit
+ In case we need communication between the client and extension:
+ https://github.com/microsoft/vscode-extension-samples/blob/main/webview-sample/src/extension.ts#L209
+ https://github.com/microsoft/vscode-extension-samples/blob/main/webview-sample/media/main.js
+ However, most likely we will only use this to reveal secrets by button
+ If we do, we must enable inline scripts to get access to the vscode API
+ https://stackoverflow.com/a/74971047 import "vscode-webview"
+
+ https://stackoverflow.com/questions/54632431/vscode-api-read-clipboard-text-content
+ https://vshaxe.github.io/vscode-extern/vscode/Clipboard.html
+
+ see for icon buton https://github.com/microsoft/vscode-webview-ui-toolkit/blob/main/src/button/README.md#start-icon
+ for dropdown https://github.com/microsoft/vscode-webview-ui-toolkit/blob/main/src/dropdown/README.md
+ */
+
+/**
+ * Copy the text content of an element to the clipboard (VS Code webview friendly)
+ * @param el - HTMLElement to copy from
+ */
+function copyElementText(element: HTMLElement) {
+	if (!element) return
+	// TODO this doesn't work, use Clipboard.writeText when const vscode = acquireVsCodeApi(); becomes available
+	navigator.clipboard.writeText(element.textContent || '')
+		.then(() => console.log('Copied!'))
+		.catch(error => console.error('Copy failed:', error))
+}
+
+function createElement<TElement extends HTMLElement>(element: string, properties?: Partial<TElement>) {
+	return Object.assign(document.createElement(element) as TElement, properties ?? {})
+}
+
+export class CopyButton extends HTMLElement {
+	private valueElement: HTMLElement | undefined
+
+	private root: ShadowRoot
+
+	private mainButton: Button
+	private copyUnmaskedButton: Button
+	private dropdownToggle: Button
+
+	private dropdownMenu: HTMLElement
+	private splitContainer: HTMLElement
+
+	constructor() {
+		super()
+
+		this.root = this.attachShadow({ mode: 'open' })
+
+		this.splitContainer = createElement('div', { className: 'split-button' })
+		this.splitContainer.setAttribute('role', 'group')
+		this.splitContainer.setAttribute('aria-label', 'Copy value')
+
+		// Main "Copy" button
+		this.mainButton = createElement<Button>('vscode-button', {
+			textContent: 'Copy',
+			type: 'button',
+			appearance: 'secondary',
+			className: 'main-button',
+		})
+		this.mainButton.setAttribute('aria-label', 'Copy value to clipboard')
+		this.mainButton.append(createElement('span', {
+			slot: 'start',
+			innerHTML: copyIconSvg,
+		}))
+		this.mainButton.addEventListener('click', _event => this.copyDefault())
+		this.splitContainer.append(this.mainButton)
+
+		// Dropdown toggle button with chevron-down icon
+		this.dropdownToggle = createElement<Button>('vscode-button', {
+			type: 'button',
+			title: 'More options',
+			appearance: 'secondary',
+			className: 'dropdown-toggle',
+			innerHTML: chevronDownSvg,
+		})
+		this.dropdownToggle.setAttribute('aria-label', 'More copy options')
+		this.dropdownToggle.setAttribute('aria-haspopup', 'menu')
+		this.dropdownToggle.setAttribute('aria-expanded', 'false')
+		this.splitContainer.append(this.dropdownToggle)
+
+		// Dropdown menu with "Copy unmasked" option
+		this.dropdownMenu = createElement('div', {
+			className: 'dropdown-menu',
+		})
+		this.dropdownMenu.setAttribute('role', 'menu')
+		this.copyUnmaskedButton = createElement<Button>('vscode-button', {
+			className: 'dropdown-item',
+			textContent: 'Copy unmasked',
+			appearance: 'secondary',
+		})
+		this.copyUnmaskedButton.setAttribute('role', 'menuitem')
+		this.dropdownMenu.append(this.copyUnmaskedButton)
+
+		// Toggle dropdown on chevron click
+		this.dropdownToggle.addEventListener('click', (event) => {
+			event.stopPropagation()
+			if (this.dropdownMenu.classList.contains('open')) {
+				this.closeDropdown()
+			}
+			else {
+				this.openDropdown()
+			}
+		})
+
+		this.copyUnmaskedButton.addEventListener('click', (event) => {
+			event.stopPropagation()
+			this.copyUnmasked()
+		})
+
+		// Close dropdown when clicking outside
+		document.addEventListener('click', () => {
+			this.closeDropdown()
+		})
+		window.addEventListener('blur', () => {
+			this.closeDropdown()
+		})
+
+		this.root.addEventListener('click', (event) => {
+			if (event.target !== this.dropdownToggle && !this.dropdownToggle.contains(event.target as Node)) {
+				this.closeDropdown()
+			}
+		})
+	}
+
+	connectedCallback() {
+		const valueSelector = this.getAttribute('for')
+		requestAnimationFrame(() => {
+			this.valueElement = valueSelector ? document.querySelector(valueSelector) : undefined
+			if (!this.valueElement) {
+				this.mainButton.disabled = true
+				this.mainButton.title = 'Copy unavailable'
+				this.copyUnmaskedButton.disabled = true
+				this.dropdownToggle.disabled = true
+				this.dropdownToggle.title = 'Copy unavailable'
+			}
+
+			const type = this.getAttribute('type')
+			if (type) {
+				this.mainButton.title = `Copy the ${type} with sensitive values masked`
+				this.copyUnmaskedButton.title = `Copy the ${type} with sensitive values UNMASKED`
+				this.splitContainer.setAttribute('aria-label', `Copy ${type}`)
+				this.mainButton.setAttribute('aria-label', `Copy ${type} to clipboard`)
+			}
+			// Scoped styles for the split button layout, loaded via <link> to comply with CSP
+			// href/nonce are set in connectedCallback, since attributes aren't available in the constructor
+			this.root.append(createElement<HTMLLinkElement>('link', {
+				rel: 'stylesheet',
+				href: this.getAttribute('style-src'),
+				nonce: this.getAttribute('style-nonce'),
+				// Only once the styles are loaded do we show the button
+				onload: () => {
+					this.root.append(this.splitContainer)
+					this.root.append(this.dropdownMenu)
+				},
+			}))
+		})
+	}
+
+	openDropdown() {
+		this.dropdownMenu.classList.add('open')
+		this.dropdownToggle.setAttribute('aria-expanded', 'true')
+	}
+
+	closeDropdown() {
+		this.dropdownMenu.classList.remove('open')
+		this.dropdownToggle.setAttribute('aria-expanded', 'false')
+	}
+
+	copyDefault() {
+		if (!this.valueElement) return
+		// TODO, I guess postmessage, use vscode clipboard api
+		console.log('Copy button clicked!')
+		copyElementText(this.valueElement)
+		// TODO show toast in postmessage
+	}
+
+	copyUnmasked() {
+		if (!this.valueElement) return
+		// TODO, I guess postmessage, use vscode clipboard api
+		console.log('Copy unmasked button clicked!')
+		copyElementText(this.valueElement)
+		// TODO show toast in postmessage instead
+		this.closeDropdown()
+	}
+}
